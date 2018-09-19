c8e09e37026948e3c57dd4501f1a86b678a2261d
Dasha <dashahneu@gmail.com>
2018-09-19 03:03:52 +0300

    generated components

--- testTask/src/app/app.module.ts
+++ testTask/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { ClientDetailsComponent } from './client-details/client-details.component';
+import { ClientsListComponent } from './clients-list/clients-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ClientDetailsComponent,
+    ClientsListComponent
   ],
   imports: [
     BrowserModule
